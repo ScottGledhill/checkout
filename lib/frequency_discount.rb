24d85a0f7639cabc1fa82f8e5fb09319470a62ed
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-08-11 14:55:11 +0100

    discount frequency found

--- lib/frequency_discount.rb
+++ lib/frequency_discount.rb
@@ -1,14 +1,25 @@
 require 'current_items'
 
 class FrequencyDiscount
-  attr_reader :frequency_needed, :item
+  attr_reader :frequency_needed, :item, :discount
 
-  def initialize(frequency_needed, item)
+  def initialize(frequency_needed, item, discount)
     @frequency_needed = frequency_needed
     @item = item
+    @discount = discount
   end
 
   def apply_discount?(basket, discount_item)
-    basket.count(discount_item) >= frequency_needed
+    item_count(basket, discount_item) >= frequency_needed
+  end
+
+  def item_count(basket, discount_item)
+    basket.count(discount_item)
+  end
+
+  def discount(basket, discount_item)
+    if apply_discount?(basket, discount_item)
+      p item_count(basket, discount_item) * @discount
+    end
   end
 end
--- spec/frequency_discount_spec.rb
+++ spec/frequency_discount_spec.rb
@@ -2,9 +2,13 @@ require 'frequency_discount'
 
 
 describe FrequencyDiscount do
-  subject(:frequency) {described_class.new(2, "Lavender heart")}
+  subject(:frequency) {described_class.new(2, "Lavender heart", 0.75)}
 
   it 'can calculate discount if frequency needed is met' do
-    expect(frequency.apply_discount?(["Lavender heart", "Lavender heart", "Personalised cufflinks"], ("Lavender heart"))).to eq true
+    expect(frequency.discount(["Lavender heart", "Lavender heart", "Personalised cufflinks"], ("Lavender heart"))).to eq 1.5
+    expect(frequency.discount(["Lavender heart", "Lavender heart", "Lavender heart" ], ("Lavender heart"))).to eq 2.25
+
   end
+
+
 end
