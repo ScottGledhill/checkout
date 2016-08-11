f761f4fb329bf5887c79a057340c52b616d5e172
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-08-11 13:10:23 +0100

    test added to check no discount if under required amount

--- spec/percentage_discount_spec.rb
+++ spec/percentage_discount_spec.rb
@@ -4,7 +4,11 @@ require 'percentage_discount'
 describe PercentageDiscount do
   subject(:percent) {described_class.new(60,10)}
 
-  it 'can decrease total by percent given' do
+  it 'can calculate discount by percent given' do
     expect(percent.discount(70)).to eq 7
+  end
+
+  it 'doesnt calculate discount if lower than required amount' do
+    expect(percent.discount(55)).to eq nil
   end
 end
